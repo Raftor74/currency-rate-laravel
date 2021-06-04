55dbcaf1f052e9b7b8b572c4fe4035cde6795efd
Raftor74 <raftor74@mail.ru>
2021-06-04 16:41:41 +0500

    feature: Добавлен Seeder для начального пользователя. Данные пользователя вынесены в .env файл

---
+++ config/credentials.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'default_user' => [
+        'username' => env('DEFAULT_USERNAME', 'admin'),
+        'email' => env('DEFAULT_USER_EMAIL', 'admin@admin'),
+        'password' => env('DEFAULT_USER_PASSWORD', '123456'),
+    ]
+];
